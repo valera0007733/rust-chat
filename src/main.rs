8dd736ad6628a29e0bf599a0e59e79d1d0226ff8
Valeriu <valeriu.leu94@gmail.com>
2026-01-25 15:58:25 +0100

    WIP: Server listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => { handle_client(stream) },
+            Err(ref e) => {println!("{:?}", e); continue},
+        }
+    }
+}
+
+fn handle_client(stream: TcpStream) {
+    println!("Connection from {}", stream.peer_addr().unwrap());
 }
