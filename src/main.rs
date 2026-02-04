3e39874ba5b4083ee094db3d50a72cadc942f524
Valeriu Leu <valeriu.leu@modefinance.com>
2026-02-04 21:47:39 +0100

    MPSC to collect client messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::io::prelude::*;
 mod server;
 mod client;
 
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,20 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::{Receiver, Sender, channel};
 use std::thread;
 
 pub fn create_server(address: &str) {
     let listener = TcpListener::bind(address).unwrap();
 
+    let (tx, rx) = channel();
+    handle_client_messages(rx);
+
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => thread::spawn( || handle_client(stream)),
+            Ok(stream) => {
+                let thread_tx: Sender<String> = tx.clone();
+                thread::spawn(move || handle_client(stream, thread_tx))
+            }
             Err(e) => {
                 eprintln!("Could not accept connection from client: {:?}", e);
                 continue;
@@ -16,7 +23,7 @@ pub fn create_server(address: &str) {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, thread_tx: Sender<String>) {
     println!("Connection from {}", stream.peer_addr().unwrap());
     let _ = writeln!(stream, "Hello fromm server!").map_err(|err| {
         eprintln!("Could not write to client: {}", err);
@@ -31,7 +38,15 @@ fn handle_client(mut stream: TcpStream) {
                 break;
             }
             Ok(n) => {
-                println!("Received: {:?}", String::from_utf8_lossy(&buffer[..n]));
+                let received = String::from_utf8_lossy(&buffer[..n]);
+                println!("Received: {:?}", &received);
+                let tx_result = thread_tx.send(received.to_string());
+                match tx_result {
+                    Err(e) => {
+                        eprintln!("Could not send message to main thread: {:?}", e);
+                    }
+                    _ => {}
+                }
             }
             Err(e) => {
                 eprintln!("Read error: {:?}", e);
@@ -39,4 +54,17 @@ fn handle_client(mut stream: TcpStream) {
             }
         }
     }
+}
+
+fn handle_client_messages(rx: Receiver<String>) {
+    thread::spawn(move || {
+        loop {
+            match rx.recv() {
+                Ok(message) => println!("Received from client: {}", message),
+                Err(e) => {
+                    eprintln!("Receiver error: {:?}", e);
+                }
+            }
+        }
+    });
 }
