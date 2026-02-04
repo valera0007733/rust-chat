d68a625ccc66cddcd7113e04d00b5cb31d1f4e16
Valeriu Leu <valeriu.leu@modefinance.com>
2026-02-04 19:56:53 +0100

    Multithreaded chat supporting multiple client connections

--- src/client.rs
+++ src/client.rs
@@ -10,15 +10,6 @@ pub fn connect_to_server(address: &str) {
         .unwrap();
 
     loop {
-        // let mut buff = vec![0; 4096];
-        // let read = stream.read(&mut buff);
-        // match read {
-        //     Ok(_) => {
-        //         println!("{}", String::from_utf8_lossy(&buff))
-        //     }
-        //     Err(e) => {eprintln!("Could not read from server: {}", e)}
-        // }
-
         let mut input = String::new();
         stdin().read_line(&mut input).expect("Failed to read line");
         stream
